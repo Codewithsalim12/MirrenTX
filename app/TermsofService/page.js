6c7e8eb1daf8567ebd70d0a13a30bc58379bdd35
Mohammad salim <mohammadsalimmir13@gmail.com>
2025-02-21 12:01:00 +0530

    update

--- app/TermsofService/page.js
+++ app/TermsofService/page.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const TermsOfService = () => {
   return (
-    <div className="max-w-4xl mx-auto p-6 space-y-6 text-gray-700">
+    <div className="max-w-4xl mx-auto p-6 space-y-6 text-gray-900 bg-white">
       <h1 className="text-3xl font-bold text-center text-gray-900">
         Terms of Service
       </h1>
